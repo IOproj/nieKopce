45b67a121d75e7fd8938c9245a5430b1b672d29a
adamski-piotrek <adamski-piotrek@wp.pl>
2019-11-19 21:04:04 +0100

    Dodałem konfiguracje do obsługi plików .js i .css

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,11 @@
+function loginBtnFunction(){
+    alert("Przycisk do logowania");
+}
+
+function registerBtnFunction(){
+    alert("Przycisk do rejestracji");
+}
+
+function gituwaBtnFunction(){
+    alert("Piątek piąteczek piątunio");
+}
