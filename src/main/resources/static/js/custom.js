373ee1ac6393170c6e872283a2e896cbd61203a1
Fabian Berda <wanakubwa@gmail.com>
2019-12-18 01:26:06 +0100

    login dropdown prototyp + zakladki

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -16,3 +16,22 @@ function gituwaBtnFunction(){
         }
     })
 }
+
+function dropdownLoginOpen() {
+    document.getElementById("loginDropdown").classList.toggle("show");
+    console.log("AAAAA")
+}
+
+// Close the all dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
